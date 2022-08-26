7c4ffef6894e9d39ddf26a5cd1e950a3521859ce
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-27 02:36:08 +0530

    creating table in databases

--- SQLQueryBridgelabz_basic.sql
+++ SQLQueryBridgelabz_basic.sql
@@ -1,3 +1,28 @@
 --UC1-Ability to create a payroll service database-------
 create database payroll_service;
 select name from sys.databases
+use payroll_service
+------comand show all databases with size and Remark----
+EXEC sp_databases
+
+---uc2-create a employee payroll table--
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(150) not null,
+salary money not null,
+start_date date not null
+)
+
+select* from employee_payroll
+----for checking employee_payroll table created or not-----
+IF EXISTS (SELECT 1 
+           FROM INFORMATION_SCHEMA.TABLES 
+           WHERE TABLE_TYPE='BASE TABLE' 
+           AND TABLE_NAME='employee_payroll') 
+   SELECT 1 AS res ELSE SELECT 0 AS res;
+
+  
+   
+
+   
